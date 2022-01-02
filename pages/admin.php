bcbdd123921deb94ae0862bd940c6b1724b14a28
Tomeczekqq <jarnutek@gmail.com>
2022-01-02 20:13:44 +0100

    Admin init fix

---
+++ pages/admin.php
@@ -0,0 +1,51 @@
+<?php
+include ('../header.php');
+require ('./functions/database.php');
+
+if (isset($_SESSION['user']) and $_SESSION['is_admin']) {
+    $db = new DbHandler();
+    $history = $db -> getOrders();
+?>
+
+
+<h2>Wyszukaj klienta</h2>
+<form>
+    <input type="text">
+</form>
+
+
+
+<h2>Zamowienia</h2>
+<table>
+<th> Id klienta </th>
+<th> Id zamowienia </th>
+<th> Miejsce Dostawy </th>
+<th> Data Zamowienia </th>
+<th> Data Dostawy </th>
+<?php
+        foreach ($history as $order) { 
+            echo "<tr>";
+            echo "<td><a href=\"client_detail.php?id={$order['IDKlienta']}\">".$order['IDKlienta']."</td>";
+            echo "<td>".$order['IDZamowienia']."</td>";
+            echo "<td>".$order['MiejsceDostawy']."</td>";
+            echo "<td>".$order['DataZamowienia']."</td>";
+            if (isset($order['DataDostawy']))
+                echo "<td>".$order['DataDostawy']."</td>";
+            else 
+                echo "<td></td>";
+            echo "</tr>";
+            
+        };?>
+</table>
+
+
+
+
+
+<?php 
+
+include('../footer.php');
+} else {
+    header('Location: main.php' );
+}
+?>
